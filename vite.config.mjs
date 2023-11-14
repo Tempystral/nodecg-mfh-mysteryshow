18884df37e8ccfa5720c1172ce8f1a348452625b
Tempystral <elijah.doern@gmail.com>
2023-11-14 15:07:53 -0500

    Change config to match expected structure

---
+++ vite.config.mjs
@@ -0,0 +1,31 @@
+import { default as vue } from '@vitejs/plugin-vue';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
+import NodeCGPlugin from 'vite-plugin-nodecg';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig({
+  server: {
+    port: 5173,
+  },
+  plugins: [
+    vue(),
+    checker({ vueTsc: { tsconfigPath: 'tsconfig.browser.json' } }),
+    NodeCGPlugin( {
+      srcDir: "./src",
+      inputs: { 'graphics/*.{js,ts}': './src/graphics/template.html',
+                'dashboard/*.{js,ts}': './src/dashboard/template.html', }
+    } ),
+  ],
+  resolve: {
+    
+    alias: {
+      "@licenseathon-vue": `${__dirname}/src/`,
+    }
+  },
+  
+});
