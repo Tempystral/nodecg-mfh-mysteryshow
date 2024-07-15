1b655db5fac368199dd20e9a816414c23130bbb9
Tempystral <elijah.doern@gmail.com>
2024-07-14 23:17:20 -0400

    Fixed some weird shit

--- src/extension/util/util.ts
+++ src/extension/util/util.ts
@@ -1,13 +1,11 @@
+import NodeCG from "@nodecg/types";
 import clone from "clone";
 import _ from "lodash";
+import { get } from "./nodecg";
 
-export function bindReplicant(
-  vueName,
-  replicantName = vueName,
-  debounceWait = 1000
-) {
-  const replicant = nodecg.Replicant(replicantName, {
-    defaultValue: this[vueName],
+/* export function bindReplicant(vueName:string, replicantName:string = vueName, debounceWait:number = 1000) {
+  const replicant = get().Replicant(replicantName, {
+    defaultValue: vueName,
   });
   let preventSend = false;
 
@@ -16,10 +14,10 @@ export function bindReplicant(
   }, debounceWait);
 
   let readValue = (newValue) => {
-    this[vueName] = clone(newValue);
+    vueName = clone(newValue);
 
     preventSend = true;
-    this.$nextTick(() => {
+    $nextTick(() => {
       preventSend = false;
     });
   };
@@ -37,9 +35,10 @@ export function bindReplicant(
       }
     });
   });
-}
+} */
 
-export function formatTimer(time, includeMs = true, alwaysIncludeHours = true) {
+export function formatTimer(time?: number, includeMs: boolean = true, alwaysIncludeHours: boolean = true) {
+  if (!time) return "00:00:00";
   let out = "";
 
   var ms = Math.floor(time % 1000),
@@ -48,22 +47,19 @@ export function formatTimer(time, includeMs = true, alwaysIncludeHours = true) {
     h = Math.floor((time / (1000 * 60 * 60)) % 24);
 
   if (alwaysIncludeHours || h > 0) {
-    out += ("0" + h).slice(-2);
-    out += ":";
+    out += ("0" + h).slice(-2) + ":";
   }
-  out += ("0" + m).slice(-2);
-  out += ":";
+  out += ("0" + m).slice(-2) + ":";
   out += ("0" + s).slice(-2);
 
   if (includeMs) {
-    out += ".";
-    out += ("00" + ms).slice(-3);
+    out += "." + ("00" + ms).slice(-3);
   }
 
   return out;
 }
 
-export function apostrophe(s) {
+export function apostrophe(s: string) {
   if (s.slice(-1) == "s") {
     return s + "’";
   } else {
