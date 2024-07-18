f2ddfba092f1ebd4afe1fa1ef4a5f460fb08a528
Tempystral <elijah.doern@gmail.com>
2024-07-17 23:23:06 -0400

    I guess the config is stable like this? God TS is fucked up

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
 import NodeCGPlugin from 'vite-plugin-nodecg';
-import Vuetify from "vite-plugin-vuetify";
+import Vuetify from 'vite-plugin-vuetify';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -15,23 +15,23 @@ export default defineConfig({
   },
   plugins: [
     vue(),
-	Vuetify({
-    	autoImport: true,
-        styles: { configFile: './src/assets/scss/vuetify-variables.scss' },
-	}),
+    Vuetify({
+      autoImport: true,
+      styles: { configFile: './src/assets/scss/vuetify-variables.scss' },
+    }),
     checker({ vueTsc: { tsconfigPath: 'tsconfig.browser.json' } }),
-    NodeCGPlugin( {
-      srcDir: "./src",
-      inputs: { 'graphics/*.{js,ts}': './src/graphics/template.html',
-                'dashboard/*.{js,ts}': './src/dashboard/template.html', }
-    } ),
+    NodeCGPlugin({
+      srcDir: './src',
+      inputs: {
+        'graphics/*.{js,ts}': './src/graphics/template.html',
+        'dashboard/*.{js,ts}': './src/dashboard/template.html',
+      },
+    }),
   ],
   resolve: {
-
     alias: {
-      "@src": `${__dirname}/src/`,
-	  "@nodecg-mfh-mysterytournament": `${__dirname}/src/`
-    }
+      '@src': `${__dirname}/src/`,
+      '@nodecg-mfh-mysterytournament': `${__dirname}/src/`,
+    },
   },
-
 });
